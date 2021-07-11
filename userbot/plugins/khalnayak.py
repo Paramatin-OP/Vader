3f20e9418a223e6811cd9a3258fdee61d0b37fcf
Jatindalal875 <79321850+Jatindalal875@users.noreply.github.com>
2021-07-11 18:23:33 +0530

    Create khalnayak.py

---
+++ userbot/plugins/khalnayak.py
@@ -0,0 +1,64 @@
+ userbot import bot as rajop
+from userbot.utils import admin_cmd as choot
+
+x = True
+@rajop.on(choot(pattern="raj"))
+async def rajraid(event):
+    while x != False:
+       
+        await event.delete()
+        kek = event.chat_id
+        await rajop.send_message(kek, "TELEGRAM ME RAHNA HAI TO KHALNAYAK KO PAPA BOLNA PAREGA WARNA TUMHARII BHN XUDEGI AISE HI ROJ")
+        await rajop.send_message(kek, "TO BHAI KAISA HAI TERI RAND MAAA KA KAALAA BHOSDA")
+        await rajop.send_message(kek, "BAHAN K LODE APNE BAAP KHALNAYAK SE LADEGA GANDU")
+        await rajop.send_message(kek, "TERI MAA K BHOSDE ME HATH DAAL K KHOL DUNGA")
+        await rajop.send_message(kek, "BAHAN K LUND TERI RAND K KAALE BHOSDE KI SAFED JHAANT NONCH LUNGA")
+        await rajop.send_message(kek, "TERI RANDI BAHAN KI SASTI CHOOT KO FAAD K TERE GALE ME ULTI TAANG DUNGA")
+        await rajop.send_message(kek, "TERI MAA K SASTE BHOSDE TERI BAHAN KO CHOOT RAKH K CHOOS LUNGA")
+        await rajop.send_message(kek, "TERI KHANDAN KI EK EK RANDI KO LODE PE BITHA K NACHAAUNGA GANDU")
+        await rajop.send_message(kek, "KHALNAYAK BAAP H TERA")
+        await rajop.send_message(kek, "TERI MAA CHOD K RANDI BNA DIYA USKO")
+        await rajop.send_message(kek, "TERE KHANDAN KI HAR EK RAND KO APNE LODE PE BITHA K JHOOLA JHULA DIYA")
+        await rajop.send_message(kek, "AB BHI LADEGA TU APNE BAAP KHALNAYAK SE")
+        await rajop.send_message(kek, "BAHAN K LODDEEEE")
+        await rajop.send_message(kek, "SUWAR KI GAND K PISSU")
+        await rajop.send_message(kek, "CHHIPKLI KI CHOOT K MOOT")
+        await rajop.send_message(kek, "KUTTIYA KI CHOOT K DAANE")
+        await rajop.send_message(kek, "TERI MAA K SASTE BHOSDE TERI BAHAN KO CHOOT RAKH K CHOOS LUNGA")
+        await rajop.send_message(kek, "TERI RAND MUMMY KI CHOOT KA BUM BUM BHOSDA")
+        await rajop.send_message(kek, "LUND K FAKEERRRR")
+        await rajop.send_message(kek, "AAND K MARIZ")
+        await rajop.send_message(kek, "TERI MUMMY KI CHOOT ME TATA SKY LGA DALA..TO TERE BAAP KHALNAYAK KA LODA JHINGAAAA LA LA")
+        await rajop.send_message(kek, "MADRCHOD")
+        await rajop.send_message(kek, "CHUDI HUI BHOSDI K TOOTE HUYE BAAL")
+        await rajop.send_message(kek, "BHAAGGGGGG TERI MAA KI CHOOT")
+        await rajop.send_message(kek, "BAHAN K LODE")
+        await rajop.send_message(kek, "SAALE LODE K BHIKHAARI")
+        await rajop.send_message(kek, "TERI MAA KI CHOOT ME MOOT K SO JAUNGA")
+        await rajop.send_message(kek, "😂")
+        await rajop.send_message(kek, "MADRCHODDDDDD")
+        await rajop.send_message(kek, "TERI MAA CHUD GAI SASTE ME BHOSDDDDIIIIIII K")
+        await rajop.send_message(kek, "BAAP K LODE PE BETH GANDU")
+        await rajop.send_message(kek, "TERI MAA KI CHOOT KE")
+        await rajop.send_message(kek, "GHUNGHRU BANDH K CHODA")
+        await rajop.send_message(kek, "TERE PADOSI BHI PUCH RAHE H")
+        await rajop.send_message(kek, "TERI RANDI MAA BHAJAN KAR RAHI H YA KIRTAN")
+        await rajop.send_message(kek, "KITNA CHUDEGA RE TU MADRCHOD")
+        await rajop.send_message(kek, "OR TU BHN K LODE YAHAN BETH K APNE BAAP KHALNAYAK K LODE KO SEHLA RA H")
+        await rajop.send_message(kek, "TERI MAA KA BHOSDA THAKTAAAA NOI H KYA BHADWE")
+        await rajop.send_message(kek, "TERI MAA KI CHOOT ME MURGA CHHAAPP PATAAKE DAAL K BJA DU")
+        await rajop.send_message(kek, "TERI BAHAN KI GULAABI CHOOT ME NIMBOO NICHOD K CHOOS LU")
+        await rajop.send_message(kek, "TERI BUA KI CHOOT ME LODA DAAL K BETH JAU")
+        await rajop.send_message(kek, "TERI DAADI KI BUDHI BHOSDI SE USKI SAFED JHAANT NONCH K TERE BAAP K TAKLE PE CHIPKA DU")
+        await rajop.send_message(kek, "TERE KHANDAN KI SAARI RANDIYON KI GAND ME JHOOMAR TAANG K GHOOMAR PE NACHAAAUUUU MADRCHODDDDDD")
+        await rajop.send_message(kek, "KHALNAYAK TERI MAA CHOD K TERA BAAP BAN GYA")
+        await rajop.send_message(kek, "AB LUND PAKAD K CHOOS APNE BAAP KHALNAYAK KA")
+       
+       CMD_HELP.update(
+    {
+        "Raj": "**Plugin : **`KHALNAYAK `\
+        \n\n**  •  Syntax : **`.raj`\
+        \n**  •  Function : **__ Replies on chat with random abuses. __\
+        \n\n\n**Note : Spam at your own risk !!**"
+    }
+)
